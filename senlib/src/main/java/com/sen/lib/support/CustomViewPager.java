23d2f2371c84cfbd21fe7952af7f4ea6aa9c86f9
申敢闯 <912532898@qq.com>
2015-04-28 20:59:57 +0800

    save

--- senlib/src/main/java/com/sen/lib/support/CustomViewPager.java
+++ senlib/src/main/java/com/sen/lib/support/CustomViewPager.java
@@ -50,9 +50,17 @@ public class CustomViewPager extends ViewPager{
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         if (!scrollAble) {
-            return true;
+            return false;
         }
         return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        if (!scrollAble)
+            return false;
+        else
+            return super.onInterceptTouchEvent(arg0);
     }
 
     @Override

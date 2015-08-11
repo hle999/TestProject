2ad56feccb11ca4696ca4c513d77bf081871e169
hle999 <hle999@126.com>
2015-08-11 09:41:23 +0800

    save

--- app/src/main/java/com/sen/test/MainActivity.java
+++ app/src/main/java/com/sen/test/MainActivity.java
@@ -1,20 +1,28 @@
 package com.sen.test;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 
 import com.google.inject.Inject;
+import com.sen.test.service.MyService;
 import com.sen.test.ui.fragment.MainFragment;
 import com.sen.test.util.ShellUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.util.TreeSet;
 
 import cn.jpush.android.api.JPushInterface;
@@ -53,6 +61,30 @@ public class MainActivity extends BaseActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+        //消息通知栏
+        //定义NotificationManager
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+        //定义通知栏展现的内容信息
+        int icon = R.drawable.ic_launcher;
+        CharSequence tickerText = "Hello World!";
+        long when = System.currentTimeMillis();
+        Notification notification = new Notification(icon, tickerText, when);
+
+        //定义下拉通知栏时要展现的内容信息
+        Context context = getApplicationContext();
+        CharSequence contentTitle = "TestProject";
+        CharSequence contentText = "Hello World!";
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        notification.setLatestEventInfo(context, contentTitle, contentText,
+                contentIntent);
+
+        //用mNotificationManager的notify方法通知用户生成标题栏消息通知
+        mNotificationManager.notify(1, notification);
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
 
     @Override
